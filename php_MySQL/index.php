5e4bafe72c4126f91dc98907834df71266bcc92a
vitória <vivi12216@gmail.com>
2026-02-24 14:16:12 -0300

    aula-php

--- php_MySQL/index.php
+++ php_MySQL/index.php
@@ -1,11 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula de php</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+//comand ode saida
+echo "<br>****************************<br>";
+echo "<h1>ola mundo</h1>";
+
+$mensage = "<br>ola mundo!<br>";
+
+//print
+$mensage = "ola";
+if (print $mensage) {
+    echo "- mensagem exibida com sucesso!!!";
+}
+echo "<br>****************************<br>";
+?>
+<br>
+<br>
+<br>
+
+<?php_ini_loaded_file();
+    echo "<br>********************************************<br>";
+$nome = "vitoria";
+
+if ($nome == "vitoria") {
+    echo "OLA vivi, bem vinda ";
+} else {
+    echo "Ola, agora sai daqui :D";
+}
+
+echo "<br>***************************<br>";
+?>
+<br>
+<br>
+<br>
+
+<?php
+echo "<br>****************************<br>";
+
+$nome = "vitoria";
+$idade = 18;
+$altura = 1.65;
+$adress = "rua que sobe e desce que nunca aparece, 723";
+echo "<br>****************************<br>";
+
+?>
+
+
